2306cbf7639824d2cdf7cc4c4dff6c4818952020
Hyder <hyder.hashmi21@gmail.com>
2025-06-05 17:15:25 +0500

    updates userresponse + create_user routecontroller set up

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -1,10 +1,27 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-from .structs import UserCreateModel
+from .structs import UserCreateModel, UserResponse
+from .services import UserService
+from sqlmodel.ext.asyncio.session import AsyncSession
+from src.db.main import get_session
 
 
 auth_router = APIRouter()
+user_service = UserService()  # Create an instance of UserService for handling user operations
 
 
-@auth_router.post("/signup")
-async def create_user(user_data: UserCreateModel):
-    pass 
+@auth_router.post("/signup", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
+async def create_user(user_data: UserCreateModel,session: AsyncSession = Depends(get_session)):
+    
+    email = user_data.email 
+
+    user_exists = await user_service.user_exists(email, session)  # Check if the user already exists in the database
+
+    if user_exists:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="User with this email already exists."
+        )
+    
+    new_user = await user_service.create_user(user_data,session)
+
+    return new_user
--- src/auth/structs.py
+++ src/auth/structs.py
@@ -1,8 +1,22 @@
 from pydantic import BaseModel, Field
-
+import datetime
+import uuid
 
 
 class UserCreateModel(BaseModel):
     username: str = Field(max_length=50)
     email: str = Field(max_length=100)
     password: str = Field(min_length=8)
+
+
+
+class UserResponse(BaseModel):
+    id : uuid.UUID
+    username : str
+    password : str 
+    email:str  
+    first_name:str
+    last_name:str
+    is_verified:bool
+    created_at: datetime 
+    updated_at: datetime 
