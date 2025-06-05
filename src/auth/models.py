f3d6eaf4956ba7b92a49cd4d92322d210b8a1022
Hyder <hyder.hashmi21@gmail.com>
2025-06-05 15:33:37 +0500

    user model created

---
+++ src/auth/models.py
@@ -0,0 +1,27 @@
+from sqlmodel import SQLModel,Field,Column
+import sqlalchemy.dialects.postgresql as pg
+import uuid
+from datetime import datetime
+
+
+
+
+class User(SQLModel,table=True):
+    __tablename__ = "users"
+    id : uuid.UUID = Field(sa_column=Column(
+        pg.UUID,
+        nullable=False,
+        primary_key=True,
+        default=uuid.uuid4 # Automatically generate a new UUID for each user
+    ))
+    username : str
+    password : str
+    email:str  # Email should be unique, consider adding a unique constraint in the database
+    first_name:str
+    last_name:str
+    is_verified:bool = Field(default=False)  # Default to False, can be updated later
+    created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP,default=datetime.now))
+    updated_at: datetime = Field(sa_column=Column(pg.TIMESTAMP,default=datetime.now))
+
+    def __repr__(self):
+        return f"<User {self.username}>" 
