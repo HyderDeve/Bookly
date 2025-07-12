d22c0e89c4e4862c99454a4479606572f90d5718
Hyder <hyder.hashmi21@gmail.com>
2025-07-12 23:33:29 +0500

    generalized function for error handling added in errors file

--- src/__init__.py
+++ src/__init__.py
@@ -5,20 +5,7 @@ from src.reviews.routes import review_router
 from src.tags.routes import tag_router
 from contextlib import asynccontextmanager
 from src.db.main import init_db
-from src.errors import (
-    create_error_handler,
-    InsufficientPermission,
-    InvalidToken,
-    InvalidCredentials,
-    RefreshTokenRequired,
-    AccessTokenRequired,
-    UserAlreadyExists,
-    UserNotFound,
-    BookNotFound,
-    RevokedToken,
-    TagNotFound,
-    TagAlreadyExists
-)
+from src.errors import register_all_errors
 
 @asynccontextmanager
 async def life_span(app:FastAPI):
@@ -38,121 +25,7 @@ app = FastAPI(
 )
 
 #---------------------Custom Error Handlers added---------------------#
-
-app.add_exception_handler(
-    UserAlreadyExists,
-    create_error_handler(
-        status_code = status.HTTP_403_FORBIDDEN,
-        initial_detail = {
-            'message' : 'User with email already exists'
-        }
-    )
-)
-
-
-app.add_exception_handler(
-    UserNotFound,
-    create_error_handler(
-        status_code = status.HTTP_404_NOT_FOUND,
-        initial_detail = {
-            'message' : 'User not found'
-        }
-    )
-)
-
-
-app.add_exception_handler(
-    BookNotFound,
-    create_error_handler(
-        status_code = status.HTTP_404_NOT_FOUND,
-        initial_detail = {
-            'message' : 'Book not found'
-        }
-    )
-)
-
-app.add_exception_handler(
-    TagNotFound,
-    create_error_handler(
-        status_code = status.HTTP_404_NOT_FOUND,
-        initial_detail = {
-            'message' : 'Tag not found'
-        }
-    )
-)
-
-app.add_exception_handler(
-    TagAlreadyExists,
-    create_error_handler(
-        status_code = status.HTTP_403_FORBIDDEN,
-        initial_detail = {
-            'message' : 'Tag already exists'
-        }
-    )
-)
-
-app.add_exception_handler(
-    InvalidCredentials,
-    create_error_handler(
-        status_code = status.HTTP_400_BAD_REQUEST,
-        initial_detail = {
-            'message' : 'Invalid Email or Password'
-        }
-    )
-)
-
-app.add_exception_handler(
-    InvalidToken,
-    create_error_handler(
-        status_code = status.HTTP_403_FORBIDDEN,
-        initial_detail = {
-            'message' : 'Token is invalid or expired'
-        }
-    )
-)
-
-app.add_exception_handler(
-    RevokedToken,
-    create_error_handler(
-        status_code = status.HTTP_401_UNAUTHORIZED,
-        initial_detail = {
-            'message' : 'Token is either expired or has been revoked'
-        }
-    )
-)
-
-app.add_exception_handler(
-    AccessTokenRequired,
-    create_error_handler(
-        status_code = status.HTTP_403_FORBIDDEN,
-        initial_detail = {
-            'message' : 'Please provide a valid access token',
-            'Resolution' : 'Create or get a new access token'
-        }
-    )
-)
-
-app.add_exception_handler(
-    RefreshTokenRequired,
-    create_error_handler(
-        status_code = status.HTTP_403_FORBIDDEN,
-        initial_detail = {
-            'message' : 'Please provide a valid refresh token',
-            'Resolution' : 'Create or get a new refresh token'
-        }
-    )
-)
-
-app.add_exception_handler(
-    InsufficientPermission,
-    create_error_handler(
-        status_code = status.HTTP_403_FORBIDDEN,
-        initial_detail = {
-            'message' : 'Your account does not have sufficient permissions to perform this action',
-            'Resolution' : 'Upgrade your account or contact support'
-        }
-    )
-)
+register_all_errors(app)
 #---------------------Custom Error Handlers ended---------------------#
 
 
