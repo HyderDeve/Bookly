0b90ebb1b189e55eb255a75d6603992ee94b803e
Hyder <hyder.hashmi21@gmail.com>
2025-07-13 15:35:52 +0500

    ok

---
+++ src/middleware.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.requests import Request
+import time
+
+def register_middleware(app: FastAPI):
+    
+    
+    @app.middleware('http')
+    async def custom_logging(request : Request, call_next):
+        
+        start_time = time.time()
+
+        print('Before Processing', start_time)
+
+        response = await call_next(request)
+
+        processing_time = time.time() - start_time
+
+        print('After Processing', processing_time)
+        
+        return response
+
+
