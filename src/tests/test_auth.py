aaf1718d221aa04b50651c6bf4213f63ce2516b2
Hyder <hyder.hashmi21@gmail.com>
2025-08-04 16:53:25 +0500

    unit tests for signup route in auth done

---
+++ src/tests/test_auth.py
@@ -0,0 +1,31 @@
+from src.auth.structs import UserCreateModel
+
+
+auth_prefix = f'/api/v1/auth'
+
+
+
+def test_user_create(fake_session, fake_user_service, test_client):
+    
+    signup_data = {
+        "username": "saqib_raza",
+        "email": "raza_saqib@yopmail.com",
+        "password": "PrimeToBe##1",
+        "first_name": "Saqib",
+        "last_name": "Raza"
+        }
+
+
+    response = test_client.post(
+        url = f'{auth_prefix}/signup',
+        json = signup_data,
+    )
+
+    user_data = UserCreateModel(**signup_data)
+
+    assert fake_user_service.user_exists_called_once()
+    assert fake_user_service.user_exists_called_once_with(signup_data['email'], fake_session)
+    
+    assert fake_user_service.create_user_called_once()
+    assert fake_user_service.create_user_called_once_with(user_data, fake_session)
+    
