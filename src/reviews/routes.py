2850aff5ca2cded3bee75cfa58c810997fedae77
Hyder <hyder.hashmi21@gmail.com>
2025-08-08 16:41:34 +0500

    reviews docs updated

--- src/reviews/routes.py
+++ src/reviews/routes.py
@@ -13,7 +13,17 @@ review_router = APIRouter()
 access_token_bearer = AccessTokenBearer()
 
 
-@review_router.post('/book/{book_id}',response_model = ReviewResponse)
+@review_router.post('/book/{book_id}',status_code = status.HTTP_201_CREATED, response_model = ReviewResponse, responses = {
+    500:{'description' : 'Internal Server Error', 'content':{'application/json' : {'example' : 
+      {
+        'message' : "Customized Error Message"}}}},
+    403:{'description' : 'Forbidden Access', 'content':{'application/json' : {'example' : 
+      {
+        'message' : "Token is invalid or expired"}}}},
+    404:{'description' : 'Book Not Found', 'content':{'application/json' : {'example' : 
+      {
+        'message' : "Book not found"}}}}
+})
 async def add_review(book_id:str, review_data : ReviewCreateModel, current_user: User = Depends(get_current_user), session : AsyncSession = Depends(get_session)):
 
     user_email_current = current_user.email
@@ -22,14 +32,31 @@ async def add_review(book_id:str, review_data : ReviewCreateModel, current_user:
 
     return new_review
 
-@review_router.get('/', response_model = List[ReviewResponse])
+@review_router.get('/', response_model = List[ReviewResponse], responses = {
+    500:{'description' : 'Internal Server Error', 'content':{'application/json' : {'example' : 
+      {
+        'message' : "Customized Error Message"}}}},
+    403:{'description' : 'Forbidden Access', 'content':{'application/json' : {'example' : 
+      {
+        'message' : "Token is invalid or expired"}}}}
+})
 async def get_all_reviews(token_details = Depends(access_token_bearer),session : AsyncSession = Depends(get_session)):
 
     books = await review_service.get_all_reviews(session)
 
     return books
 
-@review_router.get('/{review_id}', response_model = ReviewResponse)
+@review_router.get('/{review_id}', response_model = ReviewResponse, responses = {
+    500:{'description' : 'Internal Server Error', 'content':{'application/json' : {'example' : 
+      {
+        'message' : "Customized Error Message"}}}},
+    403:{'description' : 'Forbidden Access', 'content':{'application/json' : {'example' : 
+      {
+        'message' : "Token is invalid or expired"}}}},
+    404:{'description' : 'Review Not Found', 'content':{'application/json' : {'example' : 
+      {
+        'message' : "Review not found"}}}}
+})
 async def get_review_by_id(review_id : str,  token_details = Depends(access_token_bearer), session : AsyncSession = Depends(get_session)):
 
     review = await review_service.get_review_by_id(review_id, session)
@@ -37,7 +64,27 @@ async def get_review_by_id(review_id : str,  token_details = Depends(access_toke
     return review
 
 
-@review_router.delete('/{review_id}')
+@review_router.delete('/{review_id}', responses = {
+      200: {
+            "description": "Successful Response",
+            "content": {
+                "application/json": {
+                    "example": {
+                        "message": "Review deleted successfully"
+                    }
+                }
+            }
+        },
+      500:{'description' : 'Internal Server Error', 'content':{'application/json' : {'example' : 
+      {
+        'message' : "Customized Error Message"}}}},
+      403:{'description' : 'Forbidden Access', 'content':{'application/json' : {'example' : 
+      {
+        'message' : "Token is invalid or expired"}}}},    
+      404:{'description' : 'Book Not Found', 'content':{'application/json' : {'example' : 
+      {
+        'message' : "Review not found"}}}}
+    })
 async def delete_review(review_id : str,  token_details = Depends(access_token_bearer), session : AsyncSession = Depends(get_session)):
     
     deleted_review = await review_service.delete_review(review_id, session)
@@ -45,7 +92,7 @@ async def delete_review(review_id : str,  token_details = Depends(access_token_b
     if deleted_review is None:
         raise HTTPException(
             status_code = status.HTTP_404_NOT_FOUND,
-            detail = 'Review not found'
+            detail = {'message' : 'Review not found'}
         )
     else:
         return {'message' : 'Review deleted successfully'}
